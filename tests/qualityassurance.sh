b24911656b10ae0bd1057f78844e78218b37501c
Caio Eduardo <caio.eduardo@dtidigital.com.br>
2020-02-07 17:02:13 -0300

    Error eslint

--- tests/qualityassurance.sh
+++ tests/qualityassurance.sh
@@ -3,5 +3,6 @@ cd /home/caio/Documentos/Projetos/POC_Husky_Hooks
 
 sonar-scanner   -Dsonar.projectKey=pre-push-analise   -Dsonar.sources=.   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=b5c24475ed4e2674991272ad7055ea3a2df512c9 -Dsonar.issuesReport.html.enable=true
 
-node_modules/.bin/eslint /src/*.js
+cd node_modules/.bin/
+eslint src/*.js
 
