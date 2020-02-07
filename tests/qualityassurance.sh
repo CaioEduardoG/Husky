f8aaae22bc86df7591fbc8e9583bbe1eaab1eb86
Caio Eduardo <caio.eduardo@dtidigital.com.br>
2020-02-07 17:38:31 -0300

    add scannerwork in gitignore

--- tests/qualityassurance.sh
+++ tests/qualityassurance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/caio/Documentos/Projetos/
+cd /home/caio/Documentos/Projetos/POC_Husky_Hooks/
 
 sonar-scanner   -Dsonar.projectKey=pre-push-analise   -Dsonar.sources=./POC_Husky_Hooks -Dsonar.host.url=http://localhost:9000   -Dsonar.login=b5c24475ed4e2674991272ad7055ea3a2df512c9 -Dsonar.issuesReport.html.enable=true
 
-POC_Husky_Hooks/node_modules/.bin/eslint src/*.js
+node_modules/.bin/eslint src/*.js
 
