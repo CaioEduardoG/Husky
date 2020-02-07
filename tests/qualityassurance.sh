e07d40a4613c7c3a541085a6a043792031d05078
Caio Eduardo <caio.eduardo@dtidigital.com.br>
2020-02-07 16:44:56 -0300

    Waiting for erro sonar

--- tests/qualityassurance.sh
+++ tests/qualityassurance.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/caio/Documentos/Projetos/POC_Husky_Hooks
 
-if sonar-scanner   -Dsonar.projectKey=pre-push-analise   -Dsonar.sources=.   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=b5c24475ed4e2674991272ad7055ea3a2df512c9 -Dsonar.issuesReport.html.enable=true; then
+if (sonar-scanner   -Dsonar.projectKey=pre-push-analise   -Dsonar.sources=.   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=b5c24475ed4e2674991272ad7055ea3a2df512c9 -Dsonar.issuesReport.html.enable=true); 
+then
 else
   return -1
 
